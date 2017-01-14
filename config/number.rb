350895f71e7668026a0c33a341129a1a6a91a3e4
jstory23 <jordanstory23@gmail.com>
2017-01-14 10:30:40 -0700

    implemented validations for blogs, skills, and portfolio items.

---
+++ config/number.rb
@@ -0,0 +1,7 @@
+puts "Enter a number"
+number = gets.chomp
+if number % 2 == 0
+  puts "even"
+else
+  puts "odd"
+end
---
+++ config/numbers.rb
@@ -0,0 +1,9 @@
+#!/usr/env/ruby
+
+puts "Enter a number"
+number = gets.chomp
+if number % 2 == 0
+  puts "even"
+elseif
+  puts "odd"
+end
